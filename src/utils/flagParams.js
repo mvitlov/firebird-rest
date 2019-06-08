bbd4a7d88ebf11ad32245a26ae10d9c97203ef56
Carlos Lockward <c.fernandez@gadint.com>
2019-06-07 23:47:43 -0400

    New Features!
    - Added the ability to add up to 5 databases in a single firebird-rest instance. (Databases need to be configured on the config.json file)
    -If no --db flag is provided, the program will use the config.json file automatically
    -Supports legacy functionality. The new version can be used just like the old 0.0.2 version with no issues.

--- src/utils/flagParams.js
+++ src/utils/flagParams.js
@@ -1,28 +1,163 @@
 const flags = require('flags');
+var fs = require("fs");
+const path = require('path');
+var config = fs.readFileSync(path.resolve("./utils","./config.json"));
+var jsonConfig = JSON.parse(config);
 
 flags.defineNumber('port', '4444', 'Port that app listens to');
 flags.defineString('h', 'localhost', 'Firebird Host');
 flags.defineNumber('p', '3050', 'Firebird Port');
-flags.defineString('db', '/opt/firebird/examples/empbuild/employee.fdb', 'Absolute path to Firebird Database');
+flags.defineString('db', 'NO_DATABASE', 'Absolute path to Firebird Database');
 flags.defineString('u', 'SYSDBA', 'Firebird User');
 flags.defineString('pw', 'masterkey', 'Firebird User Password');
 flags.defineString('r', null, 'Firebird User Role');
 
 flags.parse();
 
-const options = () => {
-  return {
-    host: flags.get('h'),
-    port: flags.get('p'),
-    database: flags.get('db'),
-    user: flags.get('u'),
-    password: flags.get('pw'),
-    role: flags.get('r')
+var flagMode = "False";
+var activeDBs;
+var options1;
+var options2;
+var options3;
+var options4;
+var options5;
+var serverPort1;
+var serverPort2;
+var serverPort3;
+var serverPort4;
+var serverPort5;
+
+//If the flag mode is active, fill options1 with flags
+if (flags.get('db') != 'NO_DATABASE') {
+  flagMode = "True";
+  options1 = () => {
+    return {
+      host: flags.get('h'),
+      port: flags.get('p'),
+      database: flags.get('db'),
+      user: flags.get('u'),
+      password: flags.get('pw'),
+      role: flags.get('r')
+    };
   };
-};
+  serverPort1 = () => {
+    return flags.get('port');
+  };
+  activeDBs = () => {
+    return {
+      db1: "True",
+      db2: "False",
+      db3: "False",
+      db4: "False",
+      db5: "False"
+    }
+  }
+}
+//If the flag mode is not active fill options 1 - 5 with config.json params
+else {
+  //Database 1.
+  if (jsonConfig.options1.active == "True") {
+    options1 = () => {
+      return {
+        host: jsonConfig.options1.host,
+        port: jsonConfig.options1.port,
+        database: jsonConfig.options1.database,
+        user: jsonConfig.options1.user,
+        password: jsonConfig.options1.password,
+        role: jsonConfig.options1.role
+      };
+    };
+    serverPort1 = () => {
+      return jsonConfig.options1.srvPort;
+    };
+  }
+  //Database 2.
+  if (jsonConfig.options2.active == "True") {
+    options2 = () => {
+      return {
+        host: jsonConfig.options2.host,
+        port: jsonConfig.options2.port,
+        database: jsonConfig.options2.database,
+        user: jsonConfig.options2.user,
+        password: jsonConfig.options2.password,
+        role: jsonConfig.options2.role
+      };
+    };
+    serverPort2 = () => {
+      return jsonConfig.options2.srvPort;
+    };
+  }
+  //Database 3.
+  if (jsonConfig.options3.active == "True") {
+    options3 = () => {
+      return {
+        host: jsonConfig.options3.host,
+        port: jsonConfig.options3.port,
+        database: jsonConfig.options3.database,
+        user: jsonConfig.options3.user,
+        password: jsonConfig.options3.password,
+        role: jsonConfig.options3.role
+      };
+    };
+    serverPort3 = () => {
+      return jsonConfig.options3.srvPort;
+    };
+  }
+  //Database 4.
+  if (jsonConfig.options4.active == "True") {
+    options4 = () => {
+      return {
+        host: jsonConfig.options4.host,
+        port: jsonConfig.options4.port,
+        database: jsonConfig.options4.database,
+        user: jsonConfig.options4.user,
+        password: jsonConfig.options4.password,
+        role: jsonConfig.options4.role
+      };
+    };
+    serverPort4 = () => {
+      return jsonConfig.options4.srvPort;
+    };
+  }
+  //Database 5.
+  if (jsonConfig.options5.active == "True") {
+    options5 = () => {
+      return {
+        host: jsonConfig.options5.host,
+        port: jsonConfig.options5.port,
+        database: jsonConfig.options5.database,
+        user: jsonConfig.options5.user,
+        password: jsonConfig.options5.password,
+        role: jsonConfig.options5.role
+      };
+    };
+    serverPort5 = () => {
+      return jsonConfig.options5.srvPort;
+    };
+  }
+  //Active Databases.
+  activeDBs = () => {
+    return {
+      db1: jsonConfig.options1.active,
+      db2: jsonConfig.options2.active,
+      db3: jsonConfig.options3.active,
+      db4: jsonConfig.options4.active,
+      db5: jsonConfig.options5.active
+    };
+  };
+}
 
-const serverPort = () => {
-  return flags.get('port');
+module.exports = {
+  options1: options1,
+  serverPort1: serverPort1,
+  options2: options2,
+  serverPort2: serverPort2,
+  options3: options3,
+  serverPort3: serverPort3,
+  options4: options4,
+  serverPort4: serverPort4,
+  options5: options5,
+  serverPort5: serverPort5,
+  flagMode: flagMode,
+  activeDBs: activeDBs
 };
-
-module.exports = { options: options, serverPort: serverPort };
--- src/utils/sqlQuery.js
+++ src/utils/sqlQuery.js
@@ -1,17 +1,15 @@
 const Firebird = require('node-firebird-dev');
-const Options = require('./flagParams').options();
 const convertBufferArray = require('./convertBufferArray');
 
-const sqlQuery = param => {
+var sqlQuery1 = param => {
+  const Options1 = require('./flagParams').options1();
   return (req, res) => {
     let result = [];
     const sql = req[param].sql;
-
     if (!sql) {
       return res.send(['No valid SQL query found! Please enter a valid SQL query.']);
     }
-
-    Firebird.attach(Options, (err, db) => {
+    Firebird.attach(Options1, (err, db) => {
       if (err) {
         db.detach();
         res.status(400); // BAD REQUEST
@@ -40,4 +38,156 @@ const sqlQuery = param => {
   };
 };
 
-module.exports = sqlQuery;
+var sqlQuery2 = param => {
+  const Options2 = require('./flagParams').options2();
+  return (req, res) => {
+    let result = [];
+    const sql = req[param].sql;
+    if (!sql) {
+      return res.send(['No valid SQL query found! Please enter a valid SQL query.']);
+    }
+    Firebird.attach(Options2, (err, db) => {
+      if (err) {
+        db.detach();
+        res.status(400); // BAD REQUEST
+        return res.send(`\n${err.message}\n`);
+      }
+
+      db.query(sql, (err, data) => {
+        if (err) {
+          db.detach();
+          res.status(400); // BAD REQUEST
+          return res.send(`\n${err.message}\n`);
+        }
+        db.detach();
+
+        // CONVERT RAW QUERY RESULT AND RETURN JSON
+        data.forEach(row => {
+          let tempObj = {};
+          Object.keys(row).forEach(el => {
+            tempObj[el] = convertBufferArray(row[el]);
+          });
+          result.push(tempObj);
+        });
+        return res.send(result);
+      });
+    });
+  };
+};
+
+var sqlQuery3 = param => {
+  const Options3 = require('./flagParams').options3();
+  return (req, res) => {
+    let result = [];
+    const sql = req[param].sql;
+    if (!sql) {
+      return res.send(['No valid SQL query found! Please enter a valid SQL query.']);
+    }
+    Firebird.attach(Options3, (err, db) => {
+      if (err) {
+        db.detach();
+        res.status(400); // BAD REQUEST
+        return res.send(`\n${err.message}\n`);
+      }
+
+      db.query(sql, (err, data) => {
+        if (err) {
+          db.detach();
+          res.status(400); // BAD REQUEST
+          return res.send(`\n${err.message}\n`);
+        }
+        db.detach();
+
+        // CONVERT RAW QUERY RESULT AND RETURN JSON
+        data.forEach(row => {
+          let tempObj = {};
+          Object.keys(row).forEach(el => {
+            tempObj[el] = convertBufferArray(row[el]);
+          });
+          result.push(tempObj);
+        });
+        return res.send(result);
+      });
+    });
+  };
+};
+
+var sqlQuery4 = param => {
+  const Options4 = require('./flagParams').options4();
+  return (req, res) => {
+    let result = [];
+    const sql = req[param].sql;
+    if (!sql) {
+      return res.send(['No valid SQL query found! Please enter a valid SQL query.']);
+    }
+    Firebird.attach(Options4, (err, db) => {
+      if (err) {
+        db.detach();
+        res.status(400); // BAD REQUEST
+        return res.send(`\n${err.message}\n`);
+      }
+
+      db.query(sql, (err, data) => {
+        if (err) {
+          db.detach();
+          res.status(400); // BAD REQUEST
+          return res.send(`\n${err.message}\n`);
+        }
+        db.detach();
+
+        // CONVERT RAW QUERY RESULT AND RETURN JSON
+        data.forEach(row => {
+          let tempObj = {};
+          Object.keys(row).forEach(el => {
+            tempObj[el] = convertBufferArray(row[el]);
+          });
+          result.push(tempObj);
+        });
+        return res.send(result);
+      });
+    });
+  };
+};
+
+var sqlQuery5 = param => {
+  const Options5 = require('./flagParams').options5();
+  return (req, res) => {
+    let result = [];
+    const sql = req[param].sql;
+    if (!sql) {
+      return res.send(['No valid SQL query found! Please enter a valid SQL query.']);
+    }
+    Firebird.attach(Options5, (err, db) => {
+      if (err) {
+        db.detach();
+        res.status(400); // BAD REQUEST
+        return res.send(`\n${err.message}\n`);
+      }
+
+      db.query(sql, (err, data) => {
+        if (err) {
+          db.detach();
+          res.status(400); // BAD REQUEST
+          return res.send(`\n${err.message}\n`);
+        }
+        db.detach();
+
+        // CONVERT RAW QUERY RESULT AND RETURN JSON
+        data.forEach(row => {
+          let tempObj = {};
+          Object.keys(row).forEach(el => {
+            tempObj[el] = convertBufferArray(row[el]);
+          });
+          result.push(tempObj);
+        });
+        return res.send(result);
+      });
+    });
+  };
+};
+
+module.exports.sqlQuery1 = sqlQuery1;
+module.exports.sqlQuery2 = sqlQuery2;
+module.exports.sqlQuery3 = sqlQuery3;
+module.exports.sqlQuery4 = sqlQuery4;
+module.exports.sqlQuery5 = sqlQuery5;
